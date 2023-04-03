a97570ee2a562f2796fa4e297a6d25d60abcab14
zorawan <zora0521@gmail.com>
2023-04-03 16:34:18 -0400

    add test node map

--- intro.js
+++ intro.js
@@ -10,7 +10,7 @@ var images = [
 	{ src: "listIcons/edit.svg", alt: "edit", class: "uni ske" },
 	{ src: "listIcons/calendar.svg", alt: "calendar", class: "uni ske" },
 	{ src: "listIcons/folder.svg", alt: "folder", class: "uni ske" },
-	{ src: "listIcons/hourglass.svg", alt: "hourglass", class: "ske ai" },
+	{ src: "listIcons/hourglass.svg", alt: "hourglass", class: "uni ske ai" },
 	{ src: "listIcons/image.svg", alt: "image", class: "none" },
 	{ src: "listIcons/language.svg", alt: "language", class: "ai" },
 	{ src: "listIcons/link.svg", alt: "link", class: "ai" },
@@ -82,13 +82,10 @@ var images_g = [
 ];
 
 var imgs_g = "";
-// for (var i = 0; i < images_g.length; i++) {
-// 	imgs_g += `<img src="${images_g[i].src}" alt="${images_g[i].alt}"  onmouseenter="showImgTip(this)"/>`;
-// }
 for (var i = 0; i < images_g.length; i++) {
 	imgs_g += `<img src="${images_g[i].src}" alt="${images_g[i].alt}"  
-			   onmouseenter="showImgTip(this)"
-			   onmouseleave="hideImgTip()"/>`;
+			   onmouseenter="showImgTip(event, this)"
+			/>`;
 }
 
 var currentStatus = "";
@@ -109,9 +106,212 @@ var tooltipImages = [
 	"thermometer",
 	"usb",
 ];
-var tooltipNames = ["phone_callback", "hourglass", "bluetooth"];
+
+var platformArray = [
+	{
+		name: "Edit",
+		alt: "edit",
+		desc: [
+			"Google Material Icon",
+			"Apple SF Symbols",
+			"IBM Carbon Design",
+			"Microsoft Fluent UI",
+		],
+	},
+	{
+		name: "Hourglass",
+		alt: "hourglass",
+		desc: [
+			"Google Material Icon",
+			"Apple SF Symbols",
+			"IBM Carbon Design",
+			"Microsoft Fluent UI",
+		],
+	},
+	{ name: "Comment", alt: "comment", desc: ["Google Material Icon"] },
+	{
+		name: "Share",
+		alt: "share",
+		desc: ["Google Material Icon", "IBM Carbon Design"],
+	},
+	{
+		name: "Rate",
+		alt: "star",
+		desc: ["Google Material Icon", "IBM Carbon Design"],
+	},
+	{
+		name: "Power",
+		alt: "power",
+		desc: [
+			"Google Material Icon",
+			"Apple SF Symbols",
+			"IBM Carbon Design",
+			"Microsoft Fluent UI",
+		],
+	},
+	{
+		name: "Folder",
+		alt: "folder",
+		desc: [
+			"Google Material Icon",
+			"Apple SF Symbols",
+			"IBM Carbon Design",
+			"Microsoft Fluent UI",
+		],
+	},
+	{
+		name: "Refresh",
+		alt: "refresh",
+		desc: [
+			"Google Material Icon",
+			"Apple SF Symbols",
+			"IBM Carbon Design",
+			"Microsoft Fluent UI",
+		],
+	},
+	{
+		name: "Notifications",
+		alt: "notifications",
+		desc: ["Google Material Icon", "Apple SF Symbols"],
+	},
+	{
+		name: "Setting",
+		alt: "setting",
+		desc: ["Google Material Icon", "IBM Carbon Design", "Microsoft Fluent UI"],
+	},
+	{
+		name: "Calendar",
+		alt: "calendar",
+		desc: [
+			"Google Material Icon",
+			"Apple SF Symbols",
+			"IBM Carbon Design",
+			"Microsoft Fluent UI",
+		],
+	},
+	{
+		name: "Bookmark",
+		alt: "bookmark",
+		desc: [
+			"Google Material Icon",
+			"Apple SF Symbols",
+			"IBM Carbon Design",
+			"Microsoft Fluent UI",
+		],
+	},
+	{
+		name: "Print",
+		alt: "print",
+		desc: [
+			"Google Material Icon",
+			"Apple SF Symbols",
+			"IBM Carbon Design",
+			"Microsoft Fluent UI",
+		],
+	},
+	{
+		name: "User Profile",
+		alt: "account",
+		desc: [
+			"Google Material Icon",
+			"Apple SF Symbols",
+			"IBM Carbon Design",
+			"Microsoft Fluent UI",
+		],
+	},
+];
+
+var descArray = [
+	{
+		name: "Phone Callback",
+		desc: "As more people rely on messaging and social media to communicate, traditional phone calls are becoming less common. This trend may make the phone callback icon less relevant as its purpose becomes unclear.",
+		alt: "phone_callback",
+	},
+	{
+		name: "Hourglass",
+		desc: "The hourglass symbolizes waiting and loading, but it has been replaced by other arbitrary icons (Jakob Nielsen), like spinning circles and progress bars, which I think is more related to the ideogram from Six Methods of forming Chinese characters. The physical object has also become less commonly seen, replaced by digital interfaces.",
+		alt: "hourglass",
+	},
+	{
+		name: "Bluetooth",
+		desc: "Bluetooth is a specific technology that has only been around since 2000, and people only recognize it because they need to use it to connect devices. However, this technology has limitations and can be easily replaced by others like UWB, NFC, or Wi-Fi Direct. When the technology is replaced, the icon may lose meaning or be repurposed for other things.",
+		alt: "bluetooth",
+	},
+	{
+		name: "Share",
+		desc: "The share iconography world is divided. Many share icons are based on device and platform, including this one from Android. Whatever share icon we use today can easily be replaced by voice control or gesture interaction. We are stuck with so many different share icons right now, representing that there is no suitable design to stand the test of time",
+		alt: "share",
+	},
+	{
+		name: "Language",
+		desc: "Voice control and the automatic translation may make the language icon less necessary, as communication can be detected and translated automatically. For example, in the metaverse or with a 3d immersive interaction, how you communicate can be automatically detected and converted to something others can understand.",
+		alt: "language",
+	},
+	{
+		name: "Power",
+		desc: "As more digital devices are left on all the time, the power icon may become less necessary. Why do we need a power icon to turn on/off for things?",
+		alt: "power",
+	},
+	{
+		name: "USB",
+		desc: "USB is not as popular as the floppy disk icon for saving files. Also accompanied by cloud storage, no physical saving places are needed.",
+		alt: "usb",
+	},
+	{
+		name: "SD Card",
+		desc: "Like USB, SD Card is not as popular as the floppy disk icon. It is also confused by its shape with a sim card. As the primary storage for many digital devices, SD cards can be replaced by embedded or cloud storage.",
+		alt: "sd_card",
+	},
+	{
+		name: "Attach Files",
+		desc: "A paper clip as an object to hold the attached files is a great reference icon. However, like the share icon, it can be replaced by voice control or gesture interaction. To be smarter in online communication, attached files should be done stilly.",
+		alt: "attach_file",
+	},
+	{
+		name: "Translate",
+		desc: "Same as the language icon can be replaced by voice control. It won't be necessary to be displayed in front of users.",
+		alt: "translate",
+	},
+	{
+		name: "Scan",
+		desc: "The scan is so popular nowadays. However, it is based on the QR code technology that will be replaced by automatic recognition. On the other hand, if scanning the QR code becomes permanent, people won't need an icon to trigger their actions. When they see a legend, they immediately know what to do.",
+		alt: "scan",
+	},
+	{
+		name: "Refresh",
+		desc: "Why refresh? The freezing and flashing result can be a jarring experience for users. With greater computing power and new technologies, the need for users to refresh a page to update data may be eliminated, making the refresh icon obsolete.",
+		alt: "refresh",
+	},
+	{
+		name: "Settings",
+		desc: "Finding a specific setting element is always challenging, and people rarely do it unless necessary. Mapping what people are looking for in a single icon is not ideal. With voice control and personalization, users will need less to use the setting icon to set up their device or software platforms.",
+		alt: "settings",
+	},
+	{
+		name: "Desktop",
+		desc: "As tiny and powerful chips make personal and wearable devices possible, the need for desktop computers is decreasing. There is no use case for the desktop icon unless in the computer/technology museum for exhibiting computer history.",
+		Link: "The hyperlink icon, designed for clicking and linking information, may become obsolete as new, more intuitive options emerge. The majority job for the link icon is to provide a attach option for other pieces of information. The hyperlink may disappear when users use more intuitive options like a button or the object itself. This may make the link icon either lose its meaning or become extinct.",
+		alt: "desktop",
+	},
+	{
+		name: "Link",
+		desc: "The hyperlink icon, designed for clicking and linking information, may become obsolete as new, more intuitive options emerge. The majority job for the link icon is to provide a attach option for other pieces of information. The hyperlink may disappear when users use more intuitive options like a button or the object itself. This may make the link icon either lose its meaning or become extinct.",
+		alt: "link",
+	},
+	{
+		name: "Thermometer",
+		desc: "A thermometer is an obsolete object becoming less commonly seen in daily life. The icon is a resemblance icon (Pictograms) to represent the oldest alcohol or mercury thermometer. The digital number replaced the tube, and people read the number directly. When the object loses its function, the pictograms also forfeit its meaning.",
+		alt: "thermometer",
+	},
+	{
+		name: "Print",
+		desc: "The print icon was designed based on the home office printer. Although people are using it. Less print-out documents are required because of environmental considerations. The digital trend also leads to the none paper environment for the next generation.",
+		alt: "print",
+	},
+];
+
 // Function to show the tooltip on hover
-function showImgTip(img) {
+function showImgTip(event, img) {
 	if (currentStatus == "ske") {
 		// Get the alt text of the hovered image
 		var altText = $(img).attr("alt");
@@ -129,53 +329,70 @@ function showImgTip(img) {
 			}
 		}
 		$(".img_tooltip").html(tooltipImgs);
-		var parentOffset = $(this).offset();
-		var relX = e.pageX - parentOffset.left;
-		var relY = e.pageY - parentOffset.top;
-		return $(".img_tooltip").css("left", "relX", "top", "relY");
-	} else if (currentStatus == "ai") {
+		$(".img_tooltip").show();
+		// get the mouse position relative to the viewport
+		var mouseX = event.clientX;
+		var mouseY = event.clientY;
+		var tip = $(".img_tooltip")[0];
+		tip.style.left = mouseX - 100 + "px";
+		tip.style.top = mouseY - 200 + "px";
+	} else if (currentStatus == "ai" || currentStatus == "uni") {
+		$(".description_tooltip").hide();
 		var altText = $(img).attr("alt");
-		var imageObj = images.find(function (img) {
-			return img.alt === altText;
-		});
-		var nameT = "";
+		var array = currentStatus == "ai" ? descArray : platformArray;
+		for (var descObj of array) {
+			if (altText == descObj.alt) {
+				var desc;
+				if (currentStatus == "uni") {
+					//change here for universal icon layout
+					desc = `<div class="desc"><div class="tip_title">${descObj.name}</div><div>platforms using the same shape:</div><div>${descObj.desc}</div></div>`;
+				} else {
+					desc = `<div class="desc"><div class="tip_title">${descObj.name}</div><div>Reason:</div><div>${descObj.desc}</div></div>`;
+				}
 
-		for (var j = 0; j < tooltipNames.length; j++) {
-			if (imageObj.alt === tooltipNames[j]) {
-				nameT += `<p> ${tooltipNames[j]}  <p/>`;
+				$(".description_tooltip").html(desc);
+				$(".description_tooltip").show();
+				var tip = $(".description_tooltip")[0];
+				var mouseX = event.clientX;
+				var mouseY = event.clientY;
+				tip.style.left = mouseX - 100 + "px";
+				tip.style.top = mouseY - 200 + "px";
+				break;
 			}
 		}
-		console.log(nameT);
-		$(".img_tooltip").html(nameT);
 	}
 }
-function hideImgTip() {
-	// Find the existing image tooltip element
-	var tooltipImgs = "";
-	for (var j = 0; j < tooltipImages.length; j++) {
-		if (imageObj.alt === tooltipImages[j]) {
-			tooltipImgs += `<img src="${
-				"tooltipImages/" + tooltipImages[j]
-			}.png" class="${tooltipImages[j]}"/>`;
-		}
-	}
+// function hideImgTip() {
+// 	// Find the existing image tooltip element
+// 	var tooltipImgs = "";
+// 	for (var j = 0; j < tooltipImages.length; j++) {
+// 		if (imageObj.alt === tooltipImages[j]) {
+// 			tooltipImgs += `<img src="${
+// 				"tooltipImages/" + tooltipImages[j]
+// 			}.png" class="${tooltipImages[j]}"/>`;
+// 		}
+// 	}
 
-	// If the tooltip exists, hide it
-	if (tooltipImgs) {
-		tooltip.style.display = "none";
-	}
-}
+// 	// If the tooltip exists, hide it
+// 	if (tooltipImgs) {
+// 		tooltip.style.display = "none";
+// 	}
+// }
 
 $(document).ready(function () {
-	var compensation = $(window).height() / 2;
+	var compensation = 0;
+	// var compensation = $(window).height() / 2;
 	// calculate where the sections start
 	var second = $(".second").offset().top - compensation;
 	var third = $(".third").offset().top - compensation;
 	var fourth = $(".fourth").offset().top - compensation;
 	var footer = $("footer").offset().top - compensation;
 	$(document).scroll(function () {
-		var scrollPos = $(document).scrollTop();
+		//hide all tooltips
+		$(".img_tooltip").hide();
+		$(".description_tooltip").hide();
 
+		var scrollPos = $(document).scrollTop();
 		if (scrollPos >= second && scrollPos < third) {
 			$(".section-title-underlay").text("UNIVERSAL ICON");
 			$(".section-detail-underlay").text(
